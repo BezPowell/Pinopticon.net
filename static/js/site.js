4b06f2c6eb1008bca86bf2cf8edfd8efe2c769a8
BezPowell <bez.powell@protonmail.com>
2021-01-06 09:56:25 +0000

    Move module loader into head.

--- static/js/site.js
+++ static/js/site.js
@@ -1,9 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // import SimpleLightbox js and initialize if a gallery exists on page
-    if (document.querySelector('.gallery') !== null) {
-        import('./modules/simple-lightbox.mjs')
-            .then((Lightbox) => {
-                new Lightbox.SimpleLightbox(document.querySelectorAll(".gallery-item a"));
-            });
-    }
-});
+// import SimpleLightbox js and initialize if a gallery exists on page
+if (document.querySelector('.jsLightbox') !== null) {
+    import('./modules/simple-lightbox.mjs')
+        .then((Lightbox) => {
+            new Lightbox.SimpleLightbox(document.querySelectorAll(".gallery-item a"));
+        });
+}
